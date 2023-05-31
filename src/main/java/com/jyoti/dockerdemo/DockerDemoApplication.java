0b66ef15212919591a8cea6789b82c7eb75c74ac
hp <jyotiinis18@gmail.com>
2023-05-31 08:33:37 +0530

    code

---
+++ src/main/java/com/jyoti/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,22 @@
+package com.jyoti.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+}
+
+
+	/*git init
+	git add README.md
+		git commit -m "first commit"
+		git branch -M main
+		git remote add origin https://github.com/JPrakashM/jpgithub.git
+		git push -u origin main*/
+
